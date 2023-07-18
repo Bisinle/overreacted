a2f097c7d5ebb1c3464cc3bf7652861adb9d8b8d
Bisinle <Abdiwadud.mohamedd@gmail.com>
2023-07-18 10:44:45 +0300

    cloned the OVERREACTED porfolio site belonging Dan Abramov. as a mini porject

---
+++ src/components/Article.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Article({ title, preview, minutes, date = "January 1, 1970" }) {
+   console.log(title, preview, minutes, date);
+  return (<article>
+    <h3>{title}</h3>
+    <small>{date}</small>
+    <p>{preview}</p>
+
+  </article>);
+}
+
+export default Article;
---
+++ src/components/ArticleList.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Article from "./Article";
+
+function ArticleList({ posts }) {
+  console.log(posts);
+  const post = posts.map((post) => (
+    <article key={post}>
+      <Article
+        title={post.title}
+        preview={post.preview}
+        minutes={post.minutes}
+        date={post.date}
+      />
+    </article>
+  ));
+  return <div>{post}</div>;
+}
+
+export default ArticleList;
